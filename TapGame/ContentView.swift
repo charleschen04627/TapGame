6324d58df31444c71e7db94e0ed99c49471ab29d
CharlesChen <coge04627@gmail.com>
2025-05-25 18:23:08 +0800

    Create TapGame project

---
+++ TapGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TapGame
+//
+//  Created by Charles on 25/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TapGame/TapGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapGameApp.swift
+//  TapGame
+//
+//  Created by Charles on 25/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct TapGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
