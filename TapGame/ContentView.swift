f93c88ff7de188230cfb1d92ff582def30b84f2f
CharlesChen <coge04627@gmail.com>
2025-05-25 18:45:44 +0800

    Add timer

--- TapGame/ContentView.swift
+++ TapGame/ContentView.swift
@@ -8,7 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     private let possiblePics = ["apple", "dog", "egg"]
+    
     var body: some View {
         VStack {
             Image(possiblePics[0])
